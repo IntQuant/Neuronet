17b903e561ce722487c6fc0019dee73af8051071
IQuant <quant3234@gmail.com>
2017-02-15 10:09:43 +0300

    Final?

--- INeuronet.py
+++ INeuronet.py
@@ -8,41 +8,39 @@ from keras.layers.advanced_activations import PReLU
 from keras.layers.convolutional import Convolution2D
 from keras.layers.core import Reshape, Flatten
 from keras.utils.visualize_util import plot
+from keras.optimizers import SGD
 
 import numpy as np
 
 class GetModel:
 	@staticmethod
 	def getmodel():
-		img_rows=32
-		img_cols=32
+		img_rows=28
+		img_cols=28
 		nb_classes=10
 		
-		nb_conv = 2
+		nb_conv = 3
 		nb_pool = 2
 		
 		model = Sequential()
 		
-		model.add(Convolution2D(32, nb_conv, nb_conv, border_mode='valid', input_shape=(4, img_rows, img_cols)))
+		model.add(Convolution2D(32, nb_conv, nb_conv, border_mode='valid', input_shape=(img_rows, img_cols, 1)))
 		model.add(Activation('relu'))
 		model.add(Convolution2D(32, nb_conv, nb_conv))
 		model.add(Activation('relu'))
 		model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
 		model.add(Dropout(0.25))
-
-		model.add(Flatten())
-		model.add(Dense(32*32))
-		model.add(Activation('relu'))
-		model.add(Dropout(0.5))
-	
-		model.add(Dense(256))
+		
+		model.add(Flatten())	
+		model.add(Dense(128))
 		model.add(Activation('relu'))
 		model.add(Dropout(0.5))
 
 		model.add(Dense(nb_classes))
-		model.add(Activation('sigmoid'))
+		model.add(Activation('softmax'))
 
-		model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+		model.compile(loss='categorical_crossentropy',optimizer='adadelta',metrics=['accuracy'],								 
+								 )
 		
 		plot(model, to_file='model.png')
 		
--- predicter.py
+++ predicter.py
@@ -4,50 +4,59 @@ import tkinter as tk
 from tkinter import messagebox
 
 from Localization import localization
-
-from keras.models import Sequential
-from keras.layers import Dense, Activation, normalization
-from keras.layers.pooling import MaxPooling1D
-from keras.layers.core import Flatten, Reshape
-from keras.layers.recurrent import LSTM
-from keras.layers.convolutional import Convolution1D
-from keras.utils.np_utils import to_categorical
-from keras.layers.advanced_activations import PReLU
+from INeuronet import GetModel
 import numpy as np
 
 from skimage import io, transform
 
 with open('settings','r') as f:
 	locale = f.read(2)
-	localestyle=locale+'-style'
+
+if locale not in ['ru', 'en']:
+	locale = 'en'
+localestyle=locale+'-style'
 
 ldict = localization().get()
 
 
 
 def selectfile():
-	globals()['sfile'] = tk.filedialog.askopenfilename()
+	global sfile
+	sfile = tk.filedialog.askopenfilename()
 def predict():
 	if 'sfile' in globals():
-		print(globals()['sfile'])
-		udata = io.imread(globals()['sfile'])
+		global sfile
+		print(sfile)
+		udata = io.imread(sfile)
 		udata = transform.resize(udata, output_shape=(28, 28, 4))
-		data = np.empty((28, 28), dtype = np.uint8)
+		data = np.empty((28, 28))
 		for i in range(28):
 			for j in range(28):		
-				data[i][j]=min(max(udata[i][j][3], 0), 1)*255
-		result = float(model.predict(np.array([data])))
-		intresult = int(result+0.5)
-		resrange = abs(intresult - result)
-		percvalid = 100 - ((resrange / 0.5) * 100)
-		print('Result', intresult, 'Validness', percvalid, sep='\n')
-		messagebox.showinfo(title='Predicted', message=str(intresult))
-def viev():
+				data[i][j]=min(max(udata[i][j][3], 0), 1)
+		result = model.predict(data.reshape(1, 28, 28, 1))
+		cmax = 0
+		print(result[0])
+		for i, v in enumerate(result[0]):
+			if v > result[0][cmax]:
+				cmax = i
+		
+		messagebox.showinfo(title='Predicted', message=str(cmax))
+def viev():	
 	if 'sfile' in globals():
-		udata = io.imread(globals()['sfile'])
+		global sfile
+		udata = io.imread(sfile)
 		udata = transform.resize(udata, output_shape=(28, 28, 4))
 		io.imshow(udata)
 		io.show()
+def changelocale():
+	global locale
+	with open('settings','w') as f:
+		if locale=='ru':
+			locale='en'
+		else:
+			locale='ru'
+		updatetext(mainw)
+		f.write(locale)
 
 
 	
@@ -55,9 +64,12 @@ def viev():
 def createbutton(widget, command, name):
 	widget.buttons[name] = tk.Button(widget, fg='#0ff0ff', bg='#000000')
 	widget.buttons[name]['command'] = command
-	widget.buttons[name]['text'] = ldict[locale]['b-'+name]
-	widget.buttons[name].pack(ipadx=ldict[localestyle]['b-'+name])
-
+	widget.buttons[name]['text'] = ldict[locale]['b-'+name]	
+	widget.buttons[name].pack(fill='both')
+def updatetext(widget):
+	for name in widget.buttons:
+		widget.size = 10, 10
+		widget.buttons[name]['text'] = ldict[locale]['b-'+name]		
 mainw = tk.Tk()
 mainw.title('Predicter')
 mainw.buttons = {}
@@ -65,15 +77,13 @@ mainw.buttons = {}
 createbutton(mainw, predict, 'predict')
 createbutton(mainw, viev, 'show')
 createbutton(mainw, selectfile, 'select')
+createbutton(mainw, changelocale, 'locale')
 createbutton(mainw, exit, 'exit')
 
-model = Sequential()
-model.add(Flatten(input_shape = (28, 28)))
-model.add(Dense(output_dim=28*8))
-model.add(Activation("sigmoid"))
-model.add(Dense(output_dim=1))
-model.add(PReLU())
-model.load_weights('Weights/Weights-auto5320.hd5')
+model = GetModel.getmodel()
+
+
+model.load_weights('Weights.hd5')
 io.use_plugin('matplotlib')
 
 mainw.mainloop()
