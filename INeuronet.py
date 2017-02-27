f18ff0a4b07fc742aeceeef152216acbd54b9705
IQuant <quant3234@gmail.com>
2017-02-27 16:33:19 +0300

    213

--- INeuronet.py
+++ INeuronet.py
@@ -1,8 +1,8 @@
 """
- Neuronet implimentation
+ Реализация ИНС
 """
-from keras.models import Sequential
-from keras.layers import Dense, Activation, normalization, Dropout
+from keras.models import Model
+from keras.layers import Dense, Activation, normalization, Dropout, Input
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.advanced_activations import PReLU
 from keras.layers.convolutional import Convolution2D
@@ -19,32 +19,35 @@ class GetModel:
 		img_cols=28
 		nb_classes=10
 		
+		print('Creating model')
 		
 		
 		
+		# Два свёрточных слоя с максимумом по смежным
 		
+		inp = Input((28, 28, 1))
 		
-		model = Sequential()
+		p = Convolution2D(32, 3, 3, activation='relu')(inp)
+		p = MaxPooling2D(pool_size=(2, 2))(p)
+		p = Convolution2D(32, 3, 3, activation='relu')(p)
+		p = MaxPooling2D(pool_size=(2, 2))(p)
 		
-		model.add(Convolution2D(32, 3, 3, input_shape=(28, 28, 1)))
-		model.add(Activation('relu'))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Convolution2D(32, 3, 3))
-		model.add(Activation('relu'))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		
-		model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-		model.add(Dense(64))
-		model.add(Activation('relu'))
-		model.add(Dropout(0.5))
-		model.add(Dense(64))
-		model.add(Activation('relu'))
-		model.add(Dropout(0.5))
-		model.add(Dense(10))
-		model.add(Activation('softmax'))
+		p = Flatten()(p)  # Это превращает трёхмерные карты явлений
+							  # в одномерные
 
-		model.compile(loss='categorical_crossentropy',optimizer='rmsprop',metrics=['accuracy'],								 
-								 )
+	
+		#Финальная обработка полностью связанными слоями
+	
+		p = Dense(640, activation='relu')(p)
+		p = Dropout(0.5)(p)
+		p = Dense(320, activation='relu')(p)
+		data = Dropout(0.5)(p)
+		classes = Dense(10, activation='softmax', name='classif')(data)
+		isclassified = Dense(1, activation='sigmoid', name='isclass')(data)
+		
+		model = Model(input=inp, output=[classes, isclassified])
+		
+		
 		
 		plot(model, to_file='model.png')
 		
--- wntgd.py
+++ wntgd.py
@@ -1,3 +1,5 @@
+""" Программа для обучения ИНС """
+
 import sys
 
 import numpy as np
@@ -5,49 +7,88 @@ import random
 from INeuronet import GetModel
 from PrepImg import Prepare
 from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator as datagenc
 
 seed = 7
 np.random.seed(seed)
 random.seed(seed)
 
+#Получение модели. Сама модель находится в файле
+# INeuronet.py
 model = GetModel.getmodel()
 currentbath = 0
 
+#Получение датасета
 testsx, testsy = Prepare.get()
 
-#model.load_weights('Weights.hd5')
 
-print(model.predict(testsx[0].reshape(1, 28, 28, 1)))
+#Генератор изображений
+datagen = datagenc( 
+ rotation_range = 30,
+ zoom_range = [0.9, 1.2],
+ width_shift_range=0.2,
+ height_shift_range=0.2, 
+)
+
+
+def dataprep():
+	global datagen
+	global testsx
+	global testsy
+	gen = datagen.flow(testsx, testsy, batch_size = 128)
+	
+	while True:
+		bit = random.getrandbits(1) == 1
+		#bit = False
+		if bit:
+			#print('Using generator')
+			(testx, testy) = next(gen)
+			
+			inp = {'input_1':testx}
+			out = {'classif':testy, 'isclass':np.array([1]*128)}
+			if out['classif'].shape == (128, 10):
+				yield (inp, out)
+			
+		else:
+			#print('Creating stm random')
+			rand = np.random.random((128, 28, 28, 1))
+			class_out = np.array([[0]*10]*128)
+			
+			inp = {'input_1':rand}
+			out = {'classif':class_out, 'isclass':np.array([0]*128)}
+			yield (inp, out)
+
+sh1 = sh2 = [128, 28, 28, 1]
 
 
 
-history = model.fit(
-     x=testsx,
-     y=testsy,
-     nb_epoch=20,
-     verbose=1,     
-     validation_split=0.01,
-     batch_size=64,
-     shuffle=True,
-     
+
+
+model.compile(loss=['categorical_crossentropy', 'binary_crossentropy'],optimizer='rmsprop', loss_weights=[1., 0.2])
+
+print(len(model.input_names),model.internal_input_shapes)
+print(model.output_names)
+
+history = model.fit_generator(
+     generator = dataprep(),
+     samples_per_epoch = 12800,
+     nb_epoch=10,
+     verbose=1,
  )
  
- 
+#Делаем первый слой не тренеруемым: это позволяет сделать fine-tuning 
 model.layers[0].trainable = False
 
+#Используем замедленный оптимизатор
 csgd = SGD(lr=1e-4, momentum=0.9)
 
-model.compile(loss='categorical_crossentropy',optimizer=csgd, metrics=['accuracy'])
+model.compile(loss=['categorical_crossentropy', 'binary_crossentropy'],optimizer=csgd , loss_weights=[1., 0.5])
 
-history = model.fit(
-     x=testsx,
-     y=testsy,
-     nb_epoch=40,
-     verbose=1,     
-     validation_split=0.01,
-     batch_size=32,
-     shuffle=True,
-     
+history = model.fit_generator(
+     generator = dataprep(),
+     samples_per_epoch = 12800,
+     nb_epoch=17,
+     verbose=1,
  )
 
 
